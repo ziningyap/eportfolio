fa3ae5ec63007f3e3552764dab5d630646268faa
saltypretzels <60205657+ziningyap@users.noreply.github.com>
2022-05-10 01:10:08 +0800

    navbar

--- project.js
+++ project.js
@@ -31,3 +31,13 @@ console.log(newIndex);
 img.style.backgroundImage = images[newIndex];
 caption.innerHTML = captions[newIndex];
   });
+
+
+function showMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
