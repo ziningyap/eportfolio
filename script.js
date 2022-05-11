cddb07ed5e6f3dc32734c37b3d04392fa2933f55
saltypretzels <60205657+ziningyap@users.noreply.github.com>
2022-05-11 15:56:45 +0800

    portfolio

---
+++ script.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+    document.querySelector("#tbrm").addEventListener("click", () => {
+      window.open("https://ziningyap.com/tbrm", "_self");
+    });
+    
+      document.querySelector("#track").addEventListener("click", () => {
+      window.open("https://ziningyap.com/track", "_self");
+      console.log("track");
+    });
+    
+      document.querySelector("#ifu").addEventListener("click", () => {
+      window.open("https://ziningyap.com/trulicity", "_self");
+    });
+    
+      document.querySelector("#changi").addEventListener("click", () => {
+      window.open("https://ziningyap.com/changi", "_self");
+    });
+    
+      document.querySelector("#piggy").addEventListener("click", () => {
+      window.open("https://ziningyap.com/spacepiggy", "_self");
+    });
+    
+      document.querySelector("#tui").addEventListener("click", () => {
+      window.open("https://ziningyap.com/tui-museum", "_self");
+    });
+  
+  };
+
+function showMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+      document.querySelector("#name").className += "hideName"
+    } else {
+      x.className = "topnav";
+      document.querySelector("#name").className = ""
+    }
+  }
